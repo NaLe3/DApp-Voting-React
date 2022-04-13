7c0fc75a4a5b8e17faa30cdd090c45ca0df2b350
Nathan Lebas <nathanlebas3@gmail.com>
2022-04-13 18:23:10 +0200

    update truffle-config and App.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 
 module.exports = {
 
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
   networks: {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
-    contracts_build_directory: path.join(__dirname, "client/src/contracts"),
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
     ropsten: {
