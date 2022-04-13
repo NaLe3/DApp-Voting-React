07b7a2daaa35e3de85a98893689d1261786476a2
Nathan Lebas <nathanlebas3@gmail.com>
2022-04-13 18:09:37 +0200

    unbox truffle react

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 module.exports = {
 
   networks: {
-
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    contracts_build_directory: path.join(__dirname, "client/src/contracts"),
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
