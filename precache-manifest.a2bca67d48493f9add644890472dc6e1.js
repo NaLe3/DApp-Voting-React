912371e3b38ee4127fe9b29c92884a9b43d9882c
Nathan Lebas <nathanlebas3@gmail.com>
2022-04-20 18:34:00 +0200

    Updates

---
+++ precache-manifest.a2bca67d48493f9add644890472dc6e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b110224986fc35ceb7e0374efeabfe83",
+    "url": "/DApp-Voting-React/index.html"
+  },
+  {
+    "revision": "f889060c2b282b6d4807",
+    "url": "/DApp-Voting-React/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4d13e7b98a461cff13c0",
+    "url": "/DApp-Voting-React/static/js/2.cc8c46e4.chunk.js"
+  },
+  {
+    "revision": "f889060c2b282b6d4807",
+    "url": "/DApp-Voting-React/static/js/main.7be1c5df.chunk.js"
+  },
+  {
+    "revision": "b9ffa0f5789223d12b0d",
+    "url": "/DApp-Voting-React/static/js/runtime-main.13cb83cd.js"
+  }
+]);
